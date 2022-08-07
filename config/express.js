2ae2a7a3d1bb34adc90b87cea5649eecb4080199
krishna88861 <krishna88861@gmail.com>
2022-08-07 22:32:17 +0530

    changes

--- config/express.js
+++ config/express.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
+const fs = require('fs')
 
 module.exports = function () {
     var app = express();
@@ -12,10 +13,15 @@ module.exports = function () {
     app.use(bodyParser.json({ limit: '500mb' }));
     app.use(bodyParser.urlencoded({ extended: true, limit: '500mb' }));
     
-    app.use(express.static(path.join(__dirname, '../public')));
+    app.use(express.static(path.join(root, '/public')));
+    // app.use(express.static(path.join(__dirname, '../public')));
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, '../public/index.html'));
+        fs.readFile(root + '/public/index.html', 'utf8', (err, text) => {
+            res.send(text);
+        });
+        // res.sendFile(path.join(__dirname, '../public/index.html'));
     })
+   
 
     return app;
 }
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+global.root=__dirname;
 var config = require('./config/config');
 var express = require('./config/express');
 var app = express();
